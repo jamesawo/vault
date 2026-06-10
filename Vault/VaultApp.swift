c0c3a76b02611ccab9ed50a83091088478549a68
james <james.aworo@outlook.com>
2026-06-10 16:44:16 +0200

    Initial Commit

---
+++ Vault/VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaultApp.swift
+//  Vault
+//
+//  Created by James Aworo on 10.06.26.
+//
+
+import SwiftUI
+
+@main
+struct VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
